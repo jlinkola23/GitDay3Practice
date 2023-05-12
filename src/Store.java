505ef3ab50a92fdd18958c7a151967dd16de825c
julialinkola <julia.belenko@gmail.com>
2023-05-12 13:19:53 -0400

    created store file

---
+++ src/Store.java
@@ -0,0 +1,11 @@
+public class Store {
+    // Stores [] store = {
+    // new Macy's,
+    // new Bloomingdale's,
+    // new Nordstrom,
+    // new Neuman Marcus,
+    // new Saks 5th Avenue
+    // };
+
+
+}
